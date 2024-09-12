c56d14b612dfcdc744cd332f7eb90eb28c312b2e
Maria <mariavarava176@gmail.com>
2024-09-12 21:15:35 +0300

    Added Sidebar component

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -1,7 +1,33 @@
 import React from 'react';
 
+import styles from '../../styles/Sidebar.module.css';
+import { NavLink } from 'react-router-dom';
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <section className={styles.sidebar}>
+      <div className={styles.title}>CATEGORIES</div>
+      <nav>
+        <ul className={styles.menu}>
+          <li>
+            <NavLink to={`/categories/${1}`}>Link</NavLink>
+          </li>
+        </ul>
+      </nav>
+      <div className={styles.footer}>
+        <a className={styles.link} href="/help" target="_blank">
+          Help
+        </a>
+        <a
+          className={styles.link}
+          href="/terms"
+          target="_blank"
+          style={{ textDecoration: 'underline' }}>
+          Terms & Conditions
+        </a>
+      </div>
+    </section>
+  );
 };
 
 export default Sidebar;
--- src/utils/routes.js
+++ src/utils/routes.js
@@ -1,4 +1,5 @@
 export const ROUTES = {
   HOME: '/',
   CART: '/cart',
+  CATEGORIES: '/categories/:id',
 };
